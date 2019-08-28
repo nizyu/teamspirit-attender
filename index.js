c1c44cc5c4636cfff0a66db5a07193a6b81a7dfb
nizyu <en19926@gmail.com>
2019-08-29 04:22:42 +0900

    試作1号

---
+++ index.js
@@ -0,0 +1,56 @@
+const puppeteer = require('puppeteer');
+const config = require('config');
+const process = require('process');
+
+
+(async () => {
+  const browser = await puppeteer.launch({
+    args: config.proxy ? [ '--proxy-server=' + config.proxy ] : []
+  });
+
+  try {
+    const page = await browser.newPage();
+    
+    // ログイン
+    await page.goto('https://teamspirit.cloudforce.com/');
+    await page.type("input#username", config.id);
+    await page.type("input#password", config.password);
+    await page.screenshot({path: '1.png'});
+    await page.click('input#Login');
+
+    // 打刻ボタン表示
+    await page.waitFor('a[title="勤怠打刻"]', {timeout: 10000});
+    await page.screenshot({path: '2.png'});
+    // なぜかpuppeteerのclickが効かないのでDOMを掴んで直接クリック
+    await page.evaluate(() => {
+      const a = document.querySelector('a[title="勤怠打刻"]');
+      a.click();
+    });
+
+    // iframe で埋め込まれるので...
+    //await page.waitFor('iframe[title="Ts1PushTimeView"]', {timeout: 10000});
+    await page.waitFor(5000);
+    await page.screenshot({path: '3.png'});
+    const frame = await page.$('iframe[title="Ts1PushTimeView"]');
+    const frameContent = await eh.contentFrame();
+
+    switch(process.argv[2]) {
+      case 'attend':
+        const attendButton = await frameContent.$('button#pushStart')
+        await attendButton.click();
+        break;
+      case 'leave':
+        const leaveButton = await frameContent.$('button#pushEnd')
+        await leaveButton.click();
+        break;
+      default:
+        console.log('unknown command')
+    }
+    await page.waitFor(5000);
+    await page.screenshot({path: '4.png'});
+    console.log('ミッションコンプリート.')
+  } finally {
+    await browser.close();
+  }
+
+})();
