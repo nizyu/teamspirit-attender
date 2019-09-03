15a276176dfcd7d90fffc291236179e20ad8d661
nizyu <en19926@gmail.com>
2019-09-04 04:11:16 +0900

    別ボタンを利用して高速化

--- index.js
+++ index.js
@@ -2,64 +2,66 @@ const puppeteer = require('puppeteer');
 const config = require('config');
 const process = require('process');
 
+const attendBtnSelector = 'div#btnStInput.pw_btnnst';
+const leaveBtnSelector = 'div#btnEtInput.pw_btnnet';
+const stampingframeName = '06628000000tIES';
 
 (async () => {
   const browser = await puppeteer.launch({
     // ブラウザの動作を確認したいときはコメントアウト
-    //headless: false,
+    // headless: false,
     args: config.proxy ? [ '--proxy-server=' + config.proxy, '--no-sandbox' ] : [ '--no-sandbox' ]
   });
 
   try {
     const page = await browser.newPage();
+    // 打刻ボタンのiframeが描画領域に入ってないとロードされないようなので描画領域を広くとっておく
+    await page.setViewport({ width: 1920, height: 1080 });
+
     
     // ログイン
-    await page.goto('https://teamspirit.cloudforce.com/');
-    await page.type("input#username", config.id);
-    await page.type("input#password", config.password);
-    await page.click('input#Login');
+    await page.goto('https://teamspirit.cloudforce.com/')
+      .then(() => page.type("input#username", config.id))
+      .then(() => page.type("input#password", config.password))
+      .then(() => page.click('input#Login'))
+      .then(() => page.waitForNavigation());
 
-    // 打刻ボタン表示
-    await page.waitFor('a[title="勤怠打刻"]', {timeout: 10000});
-    console.log("ログイン成功しました")
-    // なぜかpuppeteerのclickが効かないのでDOMを掴んで直接クリック
-    await page.waitFor(3000);
-    await page.evaluate(() => {
-      const a = document.querySelector('a[title="勤怠打刻"]');
-      a.click();
-    }, {});
+    // ログイン正鵠確認兼打刻ボタンのiframe取得
+    const frame = await await page.frames().find(f => f.name() === stampingframeName);
+    console.log('ログインしました');
 
-    // iframe ロードまち
-    await page.waitFor(5000);
-    const frame = await page.$('iframe[title="Ts1PushTimeView"]');
-    const frameContent = await frame.contentFrame();
-
+    // 打刻
     switch(process.argv[2]) {
       case 'attend':
-        const attendButton = await frameContent.$('button#pushStart')
-        await attendButton.click();
+        await frame.waitForSelector(attendBtnSelector, {timeout: 10000, visible: true})
+          .catch(() => {
+            console.log('出勤ボタン押せません(打刻済み？)')
+            throw Error('打刻済み？')
+          });
+        await frame.click(attendBtnSelector);
+        console.log('出勤しました');
         break;
+
       case 'leave':
-        const leaveButton = await frameContent.$('button#pushEnd')
-        await leaveButton.click();
+        await frame.waitForSelector(leaveBtnSelector, {timeout: 10000, visible: true})
+          .catch(() => {
+            console.log('退勤ボタン押せません(打刻済み？)')
+            throw Error('打刻済み？')
+          });
+        await frame.click(leaveBtnSelector);
+        console.log('退勤しました');
         break;
+
       default:
         console.error('知らないコマンドです')
     }
-    console.log('打刻しました(打刻結果確認中)');
-    await frameContent
-      .waitForSelector('#PushFinish', {timeout: 5000, visible: true})
-      .then(
-        () => console.log('打刻の成功を確認できました'),
-        () => frameContent.$('#errorMsg')
-          .then(element => frameContent.evaluate(elm => elm.textContent, element))
-          .then(txt => console.log(txt))
-      );
+
+    // 打刻成功したら反映されるのを少し待つ
+    await page.waitFor(3000);
   } catch(error) {
     console.error('エラーが発生しました')
     console.error(error)
   } finally {
     await browser.close();
   }
-
 })();
